60abb92e60d22ba76fd24b118790ffd00ccd5e43
crc83 <sergiy.beley@gmail.com>
2020-10-26 21:43:56 +0200

    Test for list linking

--- trainingapp/test/org/crc83/j11streams/IntersectionOfTwoLinkedListsTest.java
+++ trainingapp/test/org/crc83/j11streams/IntersectionOfTwoLinkedListsTest.java
@@ -3,6 +3,8 @@ package org.crc83.j11streams;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntersectionOfTwoLinkedListsTest {
@@ -15,7 +17,8 @@ class IntersectionOfTwoLinkedListsTest {
             "0, [2,6,4], [1,5], 3, 2"
     })
     public void testGetIntersectionNode(int intersectVal , int[] listA, int[] listB, int skipA, int skipB) {
-
+        IntStream.of(listA).peek(System.out::println).count();
+        IntStream.of(listB).peek(System.out::println).count();
     }
 
 }
